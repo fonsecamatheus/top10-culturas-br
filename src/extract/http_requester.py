cc1dadf4d5ef03280ae294105cf7645de356f5f2
fonseca-matheus <matheusrdf1997@hotmail.com>
2024-06-27 08:12:56 -0400

    implementando http_requester

--- src/extract/http_requester.py
+++ src/extract/http_requester.py
@@ -1,13 +1,13 @@
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 import requests as rq
 
 
 class HttpRequester:
     def __init__(self) -> None:
-        self.url = 'https://apisidra.ibge.gov.br/values/t/5457/n1/all/n3/all/u/y/v/214,216,8331/p/all/c782/40099,40102,40106,40119,40122,40124,40127,40136,40139,40151,40329'
+        self.url = 'https://apisidra.ibge.gov.br/values/t/5457/n1/all/n3/all/u/y/v/214,216,8331/p/all/c782/40099,40102,40106,40119,40122,40124,40127,40136,40139,40151'
 
-    def http_response(self) -> Dict[str, Any]:
+    def http_response(self) -> List[Dict[str, Any]]:
         try:
             response = rq.get(self.url)
             response.raise_for_status()
