2244bbbc50fdfa287a824da8abad38e5f2e7800f
fonseca-matheus <matheusrdf1997@hotmail.com>
2024-06-27 16:47:40 -0400

    implementando data_transform

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import pprint
-
+from src.extract.data_extract import DataExtract
 from src.extract.http_requester import HttpRequester
+from src.transform.data_transform import DataTransform
 
 if __name__ == '__main__':
     response = HttpRequester()
-    pprint.pprint(response.http_response())
+    response = response.http_response()
+    data = DataExtract(response)
+    data_extract = data.extract_values()
+    df = DataTransform(data_extract)
+    df = df.data_transforms()
+    print(df.head())
---
+++ src/transform/data_transform.py
@@ -0,0 +1,29 @@
+from typing import Any, Dict, List
+
+import pandas as pd
+
+
+class DataTransform:
+    def __init__(self, data_extract:List[Dict[str, Any]]) -> None:
+        self.df = pd.DataFrame(data_extract)
+
+    def data_transforms(self) -> pd.DataFrame:
+        #Deletando colunas do df
+        self.df.drop(['NC', 'NN', 'MC', 'D1C', 'D2C', 'D3C', 'D4C'], axis=1, inplace=True)
+        #Renomeando as colunas do df
+        self.df.rename(columns={'MN':'UNIDADE', 'V':'VALOR', 'D1N': 'LOCAL', 'D2N':'VARIAVEL', 'D3N':'ANO', 'D4N':'PRODUTO'}, inplace=True)
+            
+        #Tratando os valores da coluna PRODUTO 
+        self.df['PRODUTO'] = self.df['PRODUTO'].str.replace(' (em caroço)', '')\
+                                               .str.replace(' (em casca)', '')\
+                                               .str.replace(' (em grão)', '')\
+                                               .str.replace(' (cacho)', '')\
+                                               .str.replace(' (em grão)', '')\
+                                               .str.replace(' Total', '')
+
+        #Tratando os valores '...' e '-' da coluna VALOR    
+        self.df['VALOR'] = self.df['VALOR'].replace('...', '0').replace('-', '0')   
+        return self.df
+
+    
+    
