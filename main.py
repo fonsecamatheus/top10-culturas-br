f194008dcb7b7b6583888962ef890344f8fa7293
fonsecamatheus <matheusrdf1997@gmail.com>
2024-07-24 15:57:31 -0400

    att main

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ from src.transform.data_transform import DataTransform
 if __name__ == '__main__':
     response = HttpRequester()
     response = response.http_response()
+
     data = DataExtract(response)
-    data_extract = data.extract_values()
-    df = DataTransform(data_extract)
-    df = df.data_transforms()
-    data_load = DataLoad(df)
-    data_load.load_data()
+    data = data.extract_values()
+
+    df = DataTransform(data)
+    dataframe, dimensions = df.data_transforms()
+
+    DataLoad(dataframe, dimensions).load_data()
+    
+
