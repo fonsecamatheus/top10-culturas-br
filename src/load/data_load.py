f8cdc8bba15be635981363a1d5863230a3177a3d
fonsecamatheus <matheusrdf1997@gmail.com>
2024-07-24 16:13:01 -0400

    att classe DataLoad

--- src/load/data_load.py
+++ src/load/data_load.py
@@ -1,17 +1,24 @@
+from typing import Dict
+
 import pandas as pd
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from utils.envs import DEST_DB
+
 
 class DataLoad:
-    def __init__(self, dataframe: pd.DataFrame) -> None:
+    def __init__(self, dataframe:pd.DataFrame, dimensions:Dict[str, pd.DataFrame]) -> None:
         self.dataframe = dataframe
-        #self.db_url = r'sqlite:///C:/Users/fonseca-matheus/Desktop/top10-culturas-br/data/top10culturasBR.sqlite'
+        self.dimensions = dimensions
+        self.db_url = DEST_DB
         self.engine = create_engine(self.db_url)
         self.Session = sessionmaker(bind=self.engine)
 
     def load_data(self) -> None:
         with self.engine.connect() as connection:
-            self.dataframe.to_sql(name='tabela_fato', con=connection, if_exists='replace', index=False)
-        print(f"Dados carregados com sucesso na tabela")
+            self.dataframe.to_sql(name='f_fato', con=connection, if_exists='replace', index=False)
+
+            for table_name, df in self.dimensions.items():
+                df.to_sql(name=table_name, con=connection, if_exists='replace', index=False)     
 
